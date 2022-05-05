161e14b2a692263ea2b4a3b54639e4ef855be183
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-05-05 11:49:18 +0530

    Did changes

--- myapi.py
+++ myapi.py
@@ -1,5 +1,8 @@
 from ast import Delete
+from unicodedata import name
 from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,9 +11,14 @@ students = {
     1: {
         "name": "john",
         "age": 17,
-        "class": "year 12"
+        "year": "year 12"
     }
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
 
 @app.get("/")
 def index():
@@ -19,3 +27,10 @@ def index():
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
+
+@app.get("/get-by-name/{student_id}")
+def get_student(*, student_id: int, name: Optional[str] = None, test : int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+        return{"Data": "Not Found"}
