58ed678d1045ec08fc4b6901dc18ae2a685dc0c2
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-05-06 09:39:45 +0530

    did changes

--- myapi.py
+++ myapi.py
@@ -20,12 +20,17 @@ class Student(BaseModel):
     age: int
     year: str
 
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
 @app.get("/")
 def index():
     return{"name":"First Date"}
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
+def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0)):
     return students[student_id]
 
 @app.get("/get-by-name/{student_id}")
@@ -34,3 +39,19 @@ def get_student(*, student_id: int, name: Optional[str] = None, test : int):
         if students[student_id]["name"] == name:
             return students[student_id]
         return{"Data": "Not Found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id : int, student: Student):
+    if student_id in students:
+        return{"Error": "Student exists"}
+
+    students[student_id] = student
+    return students[student_id]
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student does not exists"}
+
+    students[student_id] = student
+    return students[student_id]
